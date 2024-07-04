43317ed7e5b24790309e9e0fc753d09875d72248
Rui Correia <miguel@Desktop>
2024-07-04 16:43:34 +0100

    Solution: Added SearchLandingPage that pulls in the respective components. It makes use of the custom hook to fetch the data and has a custom handler to handle how the state of the filters gets updated.

---
+++ src/components/pages/SearchLandingPage.tsx
@@ -0,0 +1,43 @@
+import { ChangeEvent, useState } from 'react'
+import useFetchData from '@/hooks/useFetchData'
+import GlobalLayout from '@/components/layouts/global'
+import PropertiesDashboard from '@/components/layouts/dashboards/PropertiesDashboard'
+import SearchFilters from '@/components/organisms/filters/SearchFilters'
+import Sidebar from '@/components/molecules/global/Sidebar'
+import Form from '@/components/molecules/forms/Form/Form'
+import type { AppliedFilters, PropertyType } from '@/types'
+
+const SearchLandingPage = () => {
+    const [filters, setFilters] = useState<AppliedFilters>()
+    const { data, error, loading } = useFetchData(filters)
+
+    const setFiltersHandler = ({ target }: ChangeEvent<HTMLFormElement>) => {
+        const form = target.closest('form')
+        if (!form) return
+
+        const data = new FormData(form)
+        const bedrooms = data.getAll('bedrooms').map(Number).filter(Boolean)
+        const propertyType = data.getAll('propertyType') as PropertyType[]
+
+        setFilters({ bedrooms, propertyType })
+    }
+
+    // Designed loading dashboard.
+    if (loading) return <>Loading...</>
+
+    // Designed error dashboard.
+    if (error) return <></>
+
+    return (
+        <GlobalLayout>
+            <Sidebar>
+                <Form onChange={setFiltersHandler}>
+                    <SearchFilters filters={filters} />
+                </Form>
+            </Sidebar>
+            <PropertiesDashboard properties={data} />
+        </GlobalLayout>
+    )
+}
+
+export default SearchLandingPage
---
+++ src/components/pages/index.ts
@@ -0,0 +1,2 @@
+export { default } from './SearchLandingPage'
+export * from './SearchLandingPage'
