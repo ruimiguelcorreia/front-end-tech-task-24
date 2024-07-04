ba22e5180879a9eab3aaaa2647145ab9757cc75f
Rui Correia <miguel@Desktop>
2024-07-04 14:43:21 +0100

    Solution: Added svg parser and updated svgs to use currentColor instead of the original stroke. Updated the typography by adding the brand typeface.

--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,36 @@ module.exports = function override(config, env) {
         ...config.resolve.alias,
         '@': path.resolve(__dirname, 'src'),
     }
+
+    const svgRuleIndex = config.module.rules.findIndex(
+        (rule) => rule.test && rule.test.toString().includes('svg')
+    )
+
+    if (svgRuleIndex >= 0) {
+        config.module.rules[svgRuleIndex] = {
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                    },
+                },
+            ],
+        }
+    } else {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                    },
+                },
+            ],
+        })
+    }
+
     return config
 }
